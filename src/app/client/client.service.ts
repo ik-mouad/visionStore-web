904c9de332844e39d1825d971937b93ae86b7042
Aicha Eddahby <mouad.idrissi.khamlichi@gmail.com>
2018-05-01 22:22:11 +0100

    first commit

--- src/app/client/client.service.ts
+++ src/app/client/client.service.ts
@@ -12,7 +12,7 @@ export class ClientService {
 
     constructor(private http: HttpClient) { }
 
-    private clientUrl = 'http://visionstore.herokuapp.com/clients';
+    private clientUrl = 'https://visionstore.herokuapp.com/clients';
 
     public getClient() {
         return this.http.get(this.clientUrl);
